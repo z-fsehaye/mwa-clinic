2e8bb6c733493d35b41f1dfa7442cc367a9c50aa
HenokWelday <93111848+HenokWelday@users.noreply.github.com>
2022-03-14 03:14:51 -0500

    updated recordsRouter.js and recordsController.js

--- backend/controllers/recordsController.js
+++ backend/controllers/recordsController.js
@@ -1,5 +1,15 @@
+module.exports.addRecord = async (req, res, next) => {
+    let result = await req.db.collection('records').insertOne(req.body)
+    res.json(result.insertId);
+}
+
 module.exports.updateRecord = async (req, res, next) => {
     let record = await req.db.collection('records')
         .updateOne({ email: req.params.p_email, 'doctor.email': req.prams.doc_email }, req.body)
     res.json(record);
+}
+
+module.exports.addVisit = async (req, res, next) => {
+    await req.db.collection('records').updateOne({email: req.params.p_email}, {$push: {visits: req.body}})
+    res.json(req.body._id);
 }
--- backend/routes/recordsRouter.js
+++ backend/routes/recordsRouter.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const recordsController = require('../controllers/recordsController')
 
+router.post('/new-record',recordsController.addRecord)
 router.put('/doctor/:doc_email/update-record/:p_email', recordsController.updateRecord)
-
+router.post('/record/:p_email/new-visit', recordsController.addVisit)
 module.exports = router;
