a9ca4fc812ebb34e00f65c9c2f8435ceca4c2900
z-fsehaye <zfsehaye@miu.edu>
2022-03-14 23:24:55 -0500

    updated recordsController, usersController, recordsRouter, recordService and userService

--- frontend/src/app/user.service.ts
+++ frontend/src/app/user.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+  verifyEmail(email: string): Observable<any> {
+    if (email) {
+      return this.http.get('http://localhost:3000/api/users/checkemail/' + email);
+    }
+    return of(null)
+  }
+
+  signup(user : {}){
+    this.http.post('http://localhost:3000/api/users/signup', user).pipe(map((data:any) => {
+      if(data)return data
+      else return null
+    }))
+  }
+
+  login(user : {}){
+    this.http.post('http://localhost:3000/api/users/login', user).pipe(map((data:any) => {
+      if(data)return data
+      else return null
+    }))
+  }
 }
